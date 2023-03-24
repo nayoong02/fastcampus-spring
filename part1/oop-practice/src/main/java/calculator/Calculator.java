678f48fac07977410194f4de5459f3b9cb28530f
nayoong02 <sonyangel02@naver.com>
2023-03-24 19:45:37 +0900

    feat: 객체지향 방식으로 계산기 프로그램 구현

---
+++ part1/oop-practice/src/main/java/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class Calculator {
+    public static int calculate(int operand1, String operator, int operand2) {
+        return ArithmeticOperator.calculate(operand1, operator, operand2);
+    }
+}
