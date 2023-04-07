d4982b2ab6e8382e3edb76443d53898817356572
nayoong02 <sonyangel02@naver.com>
2023-04-07 15:51:36 +0900

    test: Reflection API로 @Controller, @Service를 가진 클래스 조회

--- part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
+++ part1/mvc-practice/src/test/java/org/example/ReflectionTest.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import org.example.annotation.Controller;
+import org.example.annotation.Service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -18,15 +21,22 @@ public class ReflectionTest {
 
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
-    @DisplayName("@Controller 애노테이션이 설정된 모든 클래스를 찾는다.")
+    @DisplayName("@Controller, @Service 애노테이션이 설정된 모든 클래스를 찾는다.")
     @Test
     void controllerScan() {
-        Reflections reflections = new Reflections("org.example"); // org.example 하위 패키지에 있는 모든 클래스를 대상으로 조회
-
-        // Controller 애노테이션을 가진 클래스 조회
-        Set<Class<?>> beans = new HashSet<>();
-        beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
 
         logger.debug("beans: [{}]", beans);
     }
+
+    private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
+        Reflections reflections = new Reflections("org.example");
+
+        Set<Class<?>> beans = new HashSet<>();
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+
+        return beans;
+    }
+
+
 }
