0d1945aabeaef4f5ff4691e054b71d8a2af2ca31
nayoong02 <sonyangel02@naver.com>
2023-04-09 16:04:06 +0900

    feat: 프런트 컨트롤러 패턴을 이용해 클라이언트 요청 받기

--- part1/mvc-framework-practice/src/main/java/org/example/mvc/DispatcherServlet.java
+++ part1/mvc-framework-practice/src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,8 +1,10 @@
 package org.example.mvc;
 
+import org.example.mvc.controller.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,8 +18,31 @@ import javax.servlet.http.HttpServletResponse;
 public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
+    private RequestMappingHandlerMapping rmhm;
+
+    @Override
+    public void init() throws ServletException {
+        rmhm = new RequestMappingHandlerMapping();
+        rmhm.init();
+    }
+
+    @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-        log.info("DispatcherServlet#service");
+        log.info("[DispatcherServlet] service started.");
+
+        try {
+            // 핸들러 매핑을 통해 핸들러 찾기
+            Controller handler = rmhm.findHandler(request.getRequestURI());
+
+            // 핸들러에게 작업 위임
+            String viewName = handler.handleRequest(request, response);
+
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+            requestDispatcher.forward(request, response);
+        } catch (Exception e) {
+            log.error("exception occured: [{}]", e.getMessage(), e);
+            throw new ServletException(e);
+        }
     }
 
 }
